5af2ed444fa091c5dc4fd5bf3e9787231337f3e8
rishabh-ranjan <mr.rishabh.ranjan@gmail.com>
2019-10-28 23:07:59 +0530

    sorting

--- Test.java
+++ Test.java
@@ -1,20 +1,14 @@
-import Util.UnorderedTriple;
+import Util.*;
 public class Test {
 	private static void println(Object obj) {
 		System.out.println(obj);
 	}
 	public static void main(String[] args) {
-		UnorderedTriple<Integer> p1 = new UnorderedTriple<Integer>(1, 2, 3);
-		UnorderedTriple<Integer> p2 = new UnorderedTriple<Integer>(4, 3, 2);
-		UnorderedTriple<Integer> p3 = new UnorderedTriple<Integer>(2, 4, 3);
-		UnorderedTriple<Integer> p4 = new UnorderedTriple<Integer>(2, 3, 1);
-		UnorderedTriple<Integer> p5 = new UnorderedTriple<Integer>(3, 4, 2);
-		UnorderedTriple<Integer> p6 = new UnorderedTriple<Integer>(3, 2, 1);
-		println(p1.compareTo(p2));
-		println(p2.compareTo(p3));
-		println(p3.compareTo(p4));
-		println(p4.compareTo(p5));
-		println(p5.compareTo(p6));
-		println(p6.compareTo(p1));
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		for (int i = 0; i < 1000000; ++i) {
+			a.add(-i);
+		}
+		a.sort();
+		println("done");
 	}
 }
--- Util/ArrayList.java
+++ Util/ArrayList.java
@@ -1,16 +1,16 @@
 package Util;
-public class ArrayList<T> {
-	private T[] arr;
+public class ArrayList<T extends Comparable<T>> {
+	private Object[] arr;
 	private int size;
 
 	public ArrayList() {
-		arr = (T[])(new Object[1]);
+		arr = new Object[1];
 		size = 0;
 	}
 
 	public void add(T e) {
 		if (arr.length == size) {
-			T[] tmp = (T[])(new Object[2*size]);
+			Object[] tmp = new Object[2*size];
 			for (int i = 0; i < size; ++i) {
 				tmp[i] = arr[i];
 			}
@@ -21,10 +21,39 @@ public class ArrayList<T> {
 	}
 
 	public T get(int i) {
-		return arr[i];
+		return (T)arr[i];
 	}
 
 	public int size() {
 		return size;
+	}
+
+	private void qsort(int s, int e) {
+		int ss = s, ee = e;
+		if (e <= s) return;
+		T p = (T)arr[(s+e)/2];
+		int m = s;
+		while (m <= e) {
+			if (((T)arr[m]).compareTo(p) < 0) {
+				Object t = arr[s];
+				arr[s] = arr[m];
+				arr[m] = t;
+				m++;
+				s++;
+			} else if (((T)arr[m]).compareTo(p) > 0) {
+				Object t = arr[m];
+				arr[m] = arr[e];
+				arr[e] = t;
+				e--;
+			} else {
+				m++;
+			}
+		}
+		qsort(ss, s-1);
+		qsort(e+1, ee);
+	}
+
+	public void sort() {
+		qsort(0, size-1);
 	}
 }
